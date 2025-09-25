88b6a64283a6a8542719da3d493419aa2fb9ce08
hiengianguyen <hiennguyen2060@gmail.com>
2025-09-25 14:05:19 +0700

    Config data for student approved list and classes

--- src/apps/controllers/ClassmateController.js
+++ src/apps/controllers/ClassmateController.js
@@ -1,10 +1,13 @@
-const { RegisteredCombinationModel, FirestoreModel } = require("../models");
+const { RegisteredCombinationModel, FirestoreModel, ClassesModel } = require("../models");
 const { CollectionNameConstant } = require("../../constants");
 
 class ClassmateController {
   constructor() {
     this.registeredCombinationsDbRef = new FirestoreModel(CollectionNameConstant.RegisteredCombinations, RegisteredCombinationModel);
+    this.classesDbRef = new FirestoreModel(CollectionNameConstant.Classes, ClassesModel);
     this.studentList = this.studentList.bind(this);
+    this.classes = this.classes.bind(this);
+    this.createClass = this.createClass.bind(this);
   }
 
   async studentList(req, res, next) {
@@ -19,6 +22,46 @@ class ClassmateController {
     } else {
       return res.json({
         redirect: "/auth/signin"
+      });
+    }
+  }
+
+  async classes(req, res, next) {
+    if (req?.cookies?.isLogin === "true") {
+      const classes = await this.classesDbRef.getAllItems({ fieldName: "name", type: "asc" });
+
+      return res.json({
+        classes: classes
+      });
+    } else {
+      res.json({
+        redirect: "/auth/signin"
+      });
+    }
+  }
+
+  async createClass(req, res, next) {
+    const { name, teacher, combination1, combination2 } = req.body;
+    const classModel = new ClassesModel(null, name, teacher, combination1, combination2, false);
+    try {
+      const ok = await this.classesDbRef.addItem(classModel);
+
+      if (ok) {
+        return res.json({
+          isSuccess: true,
+          message: "Tạo lớp học mới thành công!"
+        });
+      } else {
+        return res.json({
+          isSuccess: true,
+          message: "Tạo lớp học mới thành công!"
+        });
+      }
+    } catch (error) {
+      console.log(error);
+      return res.json({
+        isSuccess: false,
+        message: "Có lỗi khi tạo lớp. Hảy thử lại"
       });
     }
   }
