70930b1b09694282a8c1f308154c6c03e0c92792
hiengianguyen <hiennguyen2060@gmail.com>
2025-10-23 09:51:57 +0700

    Api change class

--- src/routes/classmateRoute.js
+++ src/routes/classmateRoute.js
@@ -5,6 +5,7 @@ const router = express.Router();
 router.get("/students", ClassmateController.studentList);
 router.get("/classes", ClassmateController.classes);
 router.get("/class/:id", ClassmateController.classDetail);
+router.post("/class/change", ClassmateController.classChange);
 router.post("/classes/create", ClassmateController.createClass);
 router.post("/student/sort", ClassmateController.studentSort);
 router.post("/classes/delete", ClassmateController.deleteClass);
